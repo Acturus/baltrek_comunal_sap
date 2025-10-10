7f18df57952275b7b5c17ae87ac771ec29606377
Arturo Tacuri <ar.tacuri@gmail.com>
2025-10-10 15:22:43 -0500

    puliendo busqueda

--- scripts/extractSuppliers.js
+++ scripts/extractSuppliers.js
@@ -5,13 +5,53 @@ import fs from 'fs/promises';
  * Consulta el Service Layer para obtener datos de proveedores.
  * @param {AxiosInstance} sessionInstance - Instancia de Axios con la sesión activa.
  */
+
+const camposConsultaSAP = [
+    // Primer grupo de campos
+    "CardCode",
+    "CardType",
+    "FederalTaxID",
+    "CardName",
+    "GroupCode",
+    "Country",
+    "FatherType",
+    "Valid",
+    "Frozen",
+    "PeymentMethodCode",
+    "VatGroupLatinAmerica",
+    "TypeReport",
+    "ShaamGroup",
+    "Series",
+    "UpdateDate",
+    "UpdateTime",
+    "CreateDate",
+    "CreateTime",
+
+    // Segundo grupo de campos (U_Campos)
+    "U_MSSL_BTP",
+    "U_MSSL_BTD",
+    "U_MSSL_BAP",
+    "U_MSSL_BAM",
+    "U_MSSL_BN1",
+    "U_MSSL_BN2",
+    "U_MSSL_BAR",
+    "U_MSSL_BPH",
+    "U_MSSL_BVI",
+    "U_MSSL_BBC",
+    "U_MSSL_BEP",
+    "U_MSSL_BEC",
+    "U_MSSL_BCC",
+    "U_MSSL_BCD",
+    "U_MSSL_BCV"
+];
+
 async function getSupplierData(sessionInstance) {
     // Columnas solicitadas: CardCode, RUC (FederalTaxID), Nombre SN (CardName)
-    const columnsToSelect = 'CardCode,FederalTaxID,CardName';
+    const columnsToSelect = camposConsultaSAP.join(',');
     
     // Filtro: CardType eq 'cSupplier'
     //const query = `/BusinessPartners?$filter=CardType eq 'cSupplier'&$select=${columnsToSelect}`;
-    const query = `/BusinessPartners?$top=5`;
+    const query = `/BusinessPartners?$select=${columnsToSelect}`;
 
     try {
         const response = await sessionInstance.get(query);
