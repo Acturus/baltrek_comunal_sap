a751068df7bdfc95bec3cdba9946163ffeca05d8
Arturo Tacuri <cristhiantacuri@bcp.com.pe>
2025-10-24 14:42:30 -0500

    hola

---
+++ scripts/src/models/proveedoresModel.js
@@ -0,0 +1,41 @@
+export const camposConsultaSAP = {
+    // Primer grupo de campos
+    "CardCode": "Código SN",
+    "CardType": "Tipo",
+    "FederalTaxID": "RUC",
+    "CardName": "Nombre SN",
+    "Country": "ISO3166",
+    "Valid": "Activo",
+    "Frozen": "Bloqueado",
+    "PeymentMethodCode": "Código Banco",    
+    
+    "UpdateDate": "Última Actualización SAP",
+    "UpdateTime": "",
+    "CreateDate": "Creación SAP",
+    "CreateTime": "",
+    
+    // "TypeReport": "",
+    // "Series": "",
+    // "GroupCode": "",
+    // "FatherType": "",
+    // "ShaamGroup": "",
+    // "VatGroupLatinAmerica": "",
+
+    // Segundo grupo de campos (U_Campos)
+
+    // "U_MSSL_BTP": "",
+    // "U_MSSL_BTD": "",
+    // "U_MSSL_BAP": "",
+    // "U_MSSL_BAM": "",
+    // "U_MSSL_BN1": "",
+    // "U_MSSL_BN2": "",
+    // "U_MSSL_BAR": "",
+    // "U_MSSL_BPH": "",
+    // "U_MSSL_BVI": "",
+    // "U_MSSL_BBC": "",
+    // "U_MSSL_BEP": "",
+    // "U_MSSL_BEC": "",
+    // "U_MSSL_BCC": "",
+    // "U_MSSL_BCD": "",
+    // "U_MSSL_BCV": ""
+}
